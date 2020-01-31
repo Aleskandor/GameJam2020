f41a84f60f2f0b490ee19369694068ea84fafa96
Aleskandor <37109130+Aleskandor@users.noreply.github.com>
2020-02-01 00:34:04 +0100

    Basic Movement
    Added horizontal movement and jumping.

---
+++ GameJam2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool grounded = true;
+
+    private float horizontalInput;
+    private float jumpForce = 10;
+
+    private Rigidbody playerRigidbody;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        grounded = false;
+        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            grounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            grounded = false;
+    }
+}
