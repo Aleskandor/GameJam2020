f80e11529a24864e7798c7dd4fd2db51f6b2f797
Aleskandor <37109130+Aleskandor@users.noreply.github.com>
2020-02-01 11:13:00 +0100

    Body Progression
    The robot can now pick up its body parts.

--- GameJam2020/Assets/Scripts/PlayerMovement.cs
+++ GameJam2020/Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Body parts:
     private bool normalLeg;
     private bool torso;
     private bool forkArm;
     private bool rocketLeg;
-
-    private bool grounded = true;
+    private bool grounded;
 
     private float distanceToGround;
     private float horizontalInput;
-    private float jumpForce = 10;
+    private float jumpForce;
+    private float movementSpeed;
+    private float headSpeed;
+    private float legSpeed;
+
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
+    public GameObject stage4;
+    public GameObject stage5;
 
     private Rigidbody playerRigidbody;
 
     void Start()
     {
+        normalLeg = false;
+        torso = false;
+        forkArm = false;
+        rocketLeg = false;
+        grounded = true;
+
         distanceToGround = GetComponentInChildren<Collider>().bounds.extents.y;
+        jumpForce = 10f;
+        headSpeed = 1f;
+        movementSpeed = headSpeed;
+        legSpeed = 2f;
 
         playerRigidbody = GetComponent<Rigidbody>();
     }
@@ -47,12 +64,48 @@ public class PlayerMovement : MonoBehaviour
 
     private void Movement()
     {
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * movementSpeed * Time.deltaTime);
     }
 
     private void Jump()
     {
-        grounded = false;
-        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        if (normalLeg)
+        {
+            grounded = false;
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Leg")
+        {
+            collision.gameObject.SetActive(false);
+            stage1.SetActive(false);
+            stage2.SetActive(true);
+            normalLeg = true;
+            movementSpeed = legSpeed;
+        }
+        else if (collision.gameObject.name == "Torso")
+        {
+            collision.gameObject.SetActive(false);
+            stage2.SetActive(false);
+            stage3.SetActive(true);
+            torso = true;
+        }
+        else if (collision.gameObject.name == "Fork")
+        {
+            collision.gameObject.SetActive(false);
+            stage3.SetActive(false);
+            stage4.SetActive(true);
+            forkArm = true;
+        }
+        else if (collision.gameObject.name == "Rocket")
+        {
+            collision.gameObject.SetActive(false);
+            stage4.SetActive(false);
+            stage5.SetActive(true);
+            rocketLeg = true;
+        }
     }
 }
